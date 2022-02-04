6d4e1711d46c7e29d6b6a69e6ea2007476478b3f
Anish Mishra <amishra@trellissoft.com>
2022-02-04 19:59:56 +0530

    Migrated to IPNS

--- api/app.py
+++ api/app.py
@@ -1,26 +1,44 @@
 from flask import Flask, jsonify, request
 from ticket import tambola_ticket
-from ipfs import image_to_data_url, create_ticket_image, create_metadata, optimized_ticket, upload_to_ipfs
+from ipfs import optimized_ticket, flat_ticket, ipfsmgmt, optimized_ticket, create_metadata, create_ticket_image
+from flask import Flask
+from celeryconfig import make_celery
+#from ipfs import ipfsmgmt, optimized_ticket, create_metadata, create_ticket_image
 
 # creating a Flask app
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
+
+app.config.update(
+    CELERY_BROKER_URL='amqp://guest@localhost',
+    CELERY_RESULT_BACKEND='rpc://guest@localhost'
+)
+celery = make_celery(app)
 
 @app.route('/api/v1/ticket', methods = ['GET'])
 def home():
     if(request.method == 'GET'):
         args = request.args
         game_id = args['game_id']
+        ticket_id = args['ticket_id']
 
         ticket = tambola_ticket(1)
-        create_ticket_image(ticket)
         o_ticket = optimized_ticket(ticket)
-        metadata = create_metadata(o_ticket, game_id)
-        res = upload_to_ipfs(metadata)
-        ipfs_hash_url = 'https://ipfs.io/ipfs/' + str(res.json()['value']['cid'])
-        return jsonify({'message': 'success', 'ticket': o_ticket, 'ipfs_url': ipfs_hash_url})
+        f_ticket = flat_ticket(o_ticket)
+        save_ticket.delay(ticket, game_id, ticket_id)
+        return jsonify({'ticket': f_ticket})
+
+
+@celery.task()
+def save_ticket(ticket, game_id, ticket_id):
+    create_ticket_image(ticket)
+    o_ticket = optimized_ticket(ticket)
+    metadata = create_metadata(o_ticket, game_id)
+    ipfs = ipfsmgmt(game_id, ticket_id, metadata)
+    ipfs.add()
+    ipfs.publish()
   
-  
+
 # driver function
 if __name__ == '__main__':
   
--- api/ipfs.py
+++ api/ipfs.py
@@ -1,12 +1,11 @@
-import base64
+import base64, requests, os, json, imgkit, ipfshttpclient
 from tabulate import tabulate
 from prettytable import PrettyTable, ALL
-import imgkit
 from functools import reduce
 from hashlib import sha256
-import requests
 
 API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGE0NTFDOGYzNEE5OThkNGEzQWRBOGM2YzU1M2ZGQjk2NTJhMjIzNmEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY0MzkwNjk2OTA0NiwibmFtZSI6InIyd2ViMyJ9.RKcLYO6ycY2rfh5PSNZ4LNi8qr2Nu3eKCFUMZVysraw'
+
 
 def create_ticket_image(ticket):
     tbl = tabulate(ticket, tablefmt="html")
@@ -31,9 +30,12 @@ def optimized_ticket(ticket):
     print(f'O_Ticket {otkt}')
     return otkt
 
-
-def create_metadata(o_ticket, game_id):
+def flat_ticket(o_ticket):
     flattend_ticket = reduce(lambda x, y: x+y, o_ticket)
+    return flattend_ticket
+
+def create_metadata(f_ticket, game_id):
+    flattend_ticket = reduce(lambda x, y: x+y, f_ticket)
     ticket_hash = sha256(bytes(flattend_ticket)).hexdigest()
     metadata = { "name": ticket_hash, \
         "description": 'Game # ' + str(game_id), \
@@ -41,10 +43,33 @@ def create_metadata(o_ticket, game_id):
     }
     return metadata
 
+class ipfsmgmt():
+    def __init__(self, game_id, ticket_id, metadata):
+        self._client = ipfshttpclient.connect(session=True)
+        baseDir = 'tickets'
+        IsbaseDir = os.path.exists(baseDir)
+        metadata = metadata
+        if not IsbaseDir:
+            os.makedirs('tickets')
+        gameDir = os.path.join(baseDir, str(game_id))
+        IsgameDir = os.path.exists(gameDir)
+        ticket_file = str(ticket_id) + '.json'
+        if not IsgameDir:
+            print('---------creating gamedir')
+            os.makedirs(gameDir)
+        f = open(os.path.join(gameDir, ticket_file), "x")
+        f.write(json.dumps(metadata))
+        f.close()
 
-def upload_to_ipfs(metadata):
-    url = 'https://api.nft.storage/upload'
-    bearer = 'Bearer ' + API_KEY
-    headers = {'Authorization': bearer }
-    response = requests.post(url, headers=headers, json=metadata)
-    return response
+    def add(self):
+        self.ipfs_hash = self._client.add('tickets', recursive=True)[-1]['Hash']
+        print(f'IPFS Hash: {self.ipfs_hash}')
+        return self.ipfs_hash
+    
+    def publish(self):
+        self.ipns_name = self._client.name.publish('/ipfs/' + str(self.ipfs_hash))
+        print(f'IPNS Name: {self.ipns_name}')
+        return self.ipns_name['Name']
+
+    def close(self):  # Call this when your done
+        self._client.close()
