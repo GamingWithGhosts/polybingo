a8f240b92c276488c9403d543f11e53160ee9df5
Anish Mishra <amishra@trellissoft.com>
2022-02-05 19:06:40 +0530

    Enable lipfs volume mount for docker

--- api/start.sh
+++ api/start.sh
@@ -8,5 +8,5 @@ docker rm polybingo-api ipfs
 docker build -t polybingo-api .
 
 #Start IPFS & API Docker
-docker run --name ipfs -p 4001:4001 -p 5001:5001 -p 8080:8080 -d ipfs/go-ipfs:v0.7.0 && \
+docker run --name ipfs -p 4001:4001 -p 5001:5001 -p 8080:8080 -v /root/ipfs:data/ipfs -d ipfs/go-ipfs:v0.7.0 && \
 docker run --name polybingo-api --add-host=host.docker.internal:host-gateway --network="host" -p 5000:5000 -v /root/tickets:/flaskapp/tickets polybingo-api
