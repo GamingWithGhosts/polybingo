c763f4ae80f48529ae850d159ba0f4899bdf01ce
Michael Vogt <info@michaelvogt.eu>
2022-02-05 11:51:22 +0100

    Raw form for create game with validity check

--- app/src/routes/create.svelte
+++ app/src/routes/create.svelte
@@ -7,9 +7,46 @@
     import { goto } from "$app/navigation";
 
     import Header from "@components/Header.svelte";
+
+
+    let form;
+
+
+    function handleStart() {
+        if (form.reportValidity()) {
+            goto('/play')
+        }
+    }
 </script>
+
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        width: 500px;
+        margin: 0 auto;
+    }
+</style>
 
 
 <Header hasButton="{false}" />
 
-<button on:click={() => goto('/play')}>Cancel</button>
+<form bind:this={form}>
+    <label for="name">Game name:</label>
+    <input id="name" required>
+
+    <label for="time">Start time:</label>
+    <input id="time" type="datetime-local" required />
+
+    <label for="interval">time between balls in seconds:</label>
+    <input id="interval" type="number" required />
+
+    <label for="price">Ticket price in Matic:</label>
+    <input id="price" required />
+
+    <button on:click|preventDefault={handleStart}>Start</button>
+    <button on:click={() => goto('/play')}>Cancel</button>
+</form>
