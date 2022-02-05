28b644201f47817f386898caf96dca5474f43a6b
C19h3r <karan1688.cse18@chitkara.edu.in>
2022-02-05 11:43:41 +0530

    UPDATED : UI of TICKETS

--- app/src/components/Tickets.svelte
+++ app/src/components/Tickets.svelte
@@ -2,121 +2,121 @@
   (c) 2022 Gaming With Ghosts
   This code is licensed under MIT license (see LICENSE for details)
 -->
-
 <script>
-    import { GetTicketMatrixFromArray } from '/src/utils/GetTicketMatrixFromArray';
+	import { GetTicketMatrixFromArray } from '/src/utils/GetTicketMatrixFromArray';
 
-    const matrix = GetTicketMatrixFromArray();
-    const tickets = [[...matrix.row1, ...matrix.row2, ...matrix.row3]];
+	const matrix = GetTicketMatrixFromArray();
+	const tickets = [
+		[...matrix.row1, ...matrix.row2, ...matrix.row3],
+		[...matrix.row1, ...matrix.row2, ...matrix.row3],
+		[...matrix.row1, ...matrix.row2, ...matrix.row3],
+		[...matrix.row1, ...matrix.row2, ...matrix.row3]
+	];
 
-    export let takenNumbers = [19, 74];
-    export let selectedNumbers = [];
+	export let takenNumbers = [19, 74];
+	export let selectedNumbers = [];
 
-
-    function handleNumberClick(event) {
-        const selected = Number?.parseInt(event.target.innerText);
-
-        if (takenNumbers.indexOf(selected) >= 0) {
-            selectedNumbers = [...selectedNumbers, selected];
-        }
-    }
+	function handleNumberClick(event) {
+		const selected = Number?.parseInt(event.target.innerText);
+		if (takenNumbers.indexOf(selected) >= 0) {
+			selectedNumbers = [...selectedNumbers, selected];
+		}
+	}
 </script>
 
+<div id="content">
+	<div id="header">
+		<p>Your Tickets</p>
+	</div>
+	<div id="boards">
+		{#each tickets as ticket, boardIndex}
+			<div class="board">
+				<div class="header">
+					<p>#60382</p>
+				</div>
+				<ul class="numbers" on:click={handleNumberClick} data-boardid={boardIndex}>
+					{#each ticket as item}
+						<li class:hasNumber={item !== 0} class:taken={selectedNumbers.indexOf(item) >= 0}>
+							<span>{item !== 0 ? item : ''}</span>
+						</li>
+					{/each}
+				</ul>
+			</div>
+		{/each}
+	</div>
+</div>
 
 <style>
-    #content {
-        padding: 10px;
-        background-color: #007373;
-    }
+	#content {
+		padding: 24px;
+		border-radius: 24px;
+		background-color: #77777765;
+	}
 
-    #header {
-        display: flex;
-        align-items: center;
+	#header {
+		display: flex;
+		align-items: center;
+		font-size: 36px;
+		font-weight: bold;
+		padding: 0 15px;
 
-        padding: 15px;
+		color: #292929;
+	}
 
-        color: #6198A0;
-        background-color: #126066;
-    }
+	.numbers {
+		display: grid;
+		grid-template: repeat(3, 72px) / repeat(9, 1fr);
+		column-gap: 5px;
+		row-gap: 5px;
+		font-size: 28px;
+		padding: 10px;
+		padding-inline: 10px;
 
-    .numbers {
-        display: grid;
-        grid-template: repeat(3, 48px) / repeat(9, 48px);
-        column-gap: 1px;
-        row-gap: 1px;
+		background-color: #000000;
+	}
 
-        padding-block-end: 10px;;
-        padding-inline: 10px;
+	.numbers li {
+		background-color: #525252;
+	}
 
-        background-color: #107DC6;
-    }
+	#boards .board {
+		margin-block-start: 15px;
+	}
 
-    .numbers li {
-        background-color: #3b8dc4;
-    }
+	#boards .header {
+		display: flex;
+		justify-content: flex-end;
+		align-content: center;
+		gap: 30px;
 
-    #boards .board {
-        margin-block-start: 15px;
-    }
+		padding: 10px;
+		font-size: 28px;
+		font-weight: bold;
+		color: #00ff22;
+		background-color: #48738b;
+	}
 
-    #boards .header {
-        display: flex;
-        justify-content: flex-end;
-        align-content: center;
-        gap: 30px;
+	#boards .header p:last-child {
+		padding-inline: 7px;
+	}
 
-        padding: 10px;
+	.board .numbers :global(.hasNumber) {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 
-        color: #6CC0FF;
-        background-color: #0B85CC;
-    }
+		background-color: white;
+		cursor: pointer;
+	}
 
-    #boards .header p:last-child {
-        background-color: #00a88e;
-        padding-inline: 7px;
-    }
+	.board .numbers .taken span {
+		font-size: 32px;
+		font-weight: 10px;
+		padding: 5px 10px;
+		border-radius: 45px;
+		text-align: center;
 
-    .board .numbers :global(.hasNumber) {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        background-color: white;
-        cursor: pointer;
-    }
-
-    .board .numbers .taken span {
-        width: 45px;
-        height: 45px;
-
-        border-radius: 50%;
-        text-align: center;
-
-        color: wheat;
-        background-color: green;
-    }
+		color: wheat;
+		background-color: green;
+	}
 </style>
-
-
-<div id="content">
-    <div id="header">
-        <p>Your Tickets</p>
-    </div>
-    <div id="boards">
-        {#each tickets as ticket, boardIndex}
-            <div class="board">
-                <div class="header">
-                    <p>#60382</p>
-                    <p>9p</p>
-                </div>
-                <ul class="numbers" on:click={handleNumberClick} data-boardid="{boardIndex}">
-                    {#each ticket as item}
-                        <li class:hasNumber={item !== 0} class:taken={selectedNumbers.indexOf(item) >= 0}>
-                            <span>{item !== 0 ? item : ''}</span>
-                        </li>
-                    {/each}
-                </ul>
-            </div>
-        {/each}
-    </div>
-</div>
